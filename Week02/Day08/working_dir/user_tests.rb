be9118e4d3844940da0ca5298d80a09054b766dc
Drew <drew@vancouvertechpodcast.ca>
2021-05-05 14:46:10 -0700

    Adds email_address_format test

--- Week02/Day08/working_dir/user_tests.rb
+++ Week02/Day08/working_dir/user_tests.rb
@@ -7,7 +7,7 @@ def instantiate_user(name, email_address)
     rescue NameError => e
         puts "FAIL: #{e.message}"
     else
-        puts "PASSED: instantiate_user"
+        puts "PASS: instantiate_user"
         u
     end
 end
@@ -19,7 +19,7 @@ def has_name(user)
     rescue NameError => e
         puts "FAIL: #{e.message}"
     else
-        puts "PASSED: has_name(user)"
+        puts "PASS: has_name(user)"
     end
 end
 
@@ -29,19 +29,45 @@ def has_email_address(user)
     rescue NameError => e
         puts "FAIL: #{e.message}"
     else
-        puts "PASSED: has_email_address(user)"
+        puts "PASS: has_email_address(user)"
     end
 end
 
 def email_address_format(email_address)
     # ensure that email addresses match the pattern:
     # `<string>@<string>.<string>`
+    if email_address.count('@') != 1
+        puts "FAIL: email address must contain exactly one @"
+        return
+    end
 
+    email_address_array = email_address.split('@')
+    if email_address_array.length != 2
+        puts "FAIL: email address must have a domain"
+        return
+    elsif email_address_array[1].count('.') == 0
+        puts "FAIL: email address domain must contain a '.'"
+        return
+    else
+        puts "PASS: email_address_format(email_address)"
+    end
+
+    # if email_address_array[1]
+
+    # begin
+    #     unless email_address.split('@')[1].count('.') >= 1
+    #         puts "FAIL: email address not formatted correctly"
+    #     end
+    # rescue StandardError => e
+    #     puts "FAIL: #{e.message}"
+    # else
+    #     puts "PASS: email_address_format(user)"
+    # end
 end
 
 def run_tests
     begin
-        user = instantiate_user("TestUser", "testemail")
+        user = instantiate_user("TestUser", "testemail@dot.com")
     rescue StandardError => e
         puts "FAIL: #{e.message}"
     end
@@ -54,12 +80,12 @@ end
 run_tests
 
 # test that user = User.new(name, email); user.greet => "Hello. My name is <name>!"
-username = "Dan"
-actual = user.greet
-expected = "Hello. My name is #{username}!"
+# username = "Dan"
+# actual = user.greet
+# expected = "Hello. My name is #{username}!"
 
-if actual != expected
-    # fail
-else
-    # pass
-end
+# if actual != expected
+#     # fail
+# else
+#     # pass
+# end
