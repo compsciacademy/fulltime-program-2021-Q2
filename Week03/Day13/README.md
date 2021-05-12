beb1d0929baa17aa8c55b40413f91d906969abb1
Drew Ogryzek <drew@vancouvertechpodcast.ca>
2021-05-12 15:59:17 -0700

    Adds bonus, and continues with Exercise 01

--- Week03/Day13/README.md
+++ Week03/Day13/README.md
@@ -17,3 +17,30 @@ find employees where email_address is like *@company.com
 
 Once that feature is enabled, they would like to test it out. However, before beginning alpha testing, they would also like to have the login feature enabled so employees can check their own vacation days, salaries, etc. as well as update their own email addresses. 
   
+## Bonus  
+
+Up until now, we've used the `.each` method on arrays to iterate over a list of things. When we want to return a new list, or a subset of that list, or an otherwise changed version, we have created an empty array, pushed the new elements into that array and then returned that array.
+
+E.g.
+
+```ruby
+def add_3(number_list)
+    return_array = []
+    number_list.each do |number|
+        return_array << number + 3
+    end
+    return_array
+end
+
+add_3([1, 2, 3, 4, 5])
+=> [4, 5, 6, 7, 8]
+
+# we could use `.map` to return an array with
+# the transformed data
+# e.g.
+def add_3(number_list)
+    number_list.map do |number|
+        number + 3
+    end
+end
+```
--- Week03/Day13/lol.rb
+++ Week03/Day13/lol.rb
@@ -1,21 +1,66 @@
-class Greeting
-    def initialize(greeting)
-        @greeting = greeting
-    end
+# def add_3(number_list)
+#     return_array = []
+#     number_list.each do |number|
+#         return_array << number + 3
+#     end
+#     return_array
+# end
 
-    def self.greet
-        "Greetings!"
-    end
-
-    def greet
-        @greeting
-    end
+def increment_each(number_list, num=1)
+    number_list.map { |number| number + num }
 end
 
-puts Greeting.greet
-
-g = Greeting.new("I am an instance vaiable!")
-
-puts g.greet
+puts increment_each([1, 2, 3, 4, 5], 27)
 
 
+
+
+
+
+
+
+
+
+
+
+# class Greeting
+#     def initialize(greeting)
+#         @greeting = greeting
+#     end
+
+#     def self.greet
+#         "Greetings!"
+#     end
+
+#     def greet
+#         @greeting
+#     end
+# end
+
+# puts Greeting.greet
+
+# g = Greeting.new("I am an instance vaiable!")
+
+# puts g.greet
+
+# class Employee
+#     def self.method
+#     end
+# end
+
+# Employee.method
+
+
+
+# # @employee = Employee.find(id)
+# # @employee.set_wage(25.35)
+# # @employee.save
+
+# # @employee = Employee.find(id)
+# # @employee.email = new_email
+# # @employee.save
+
+
+# Employee.get_me_an_employee_instance
+
+# employee
