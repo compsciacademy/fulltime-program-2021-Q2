002af3259989f190ebf6b551df7fbdf0a8ea65c5
Drew Ogryzek <drew@vancouvertechpodcast.ca>
2021-05-20 12:07:26 -0700

    Adds a little more time

--- Week04/Day19/Exercise02/time.rb
+++ Week04/Day19/Exercise02/time.rb
@@ -16,4 +16,13 @@ def current_time_range
   end
 end
 
-puts "Good #{current_time_range}!"
+# puts "Good #{current_time_range}!"
+
+preferred_greetings = {
+  "morning" => "Howdility morniditly doo!",
+  "afternoon" => "Hodily modily afternoon to you!",
+  "evening" => "Evenin' neighbor",
+  "night" => "Moon sure is bright"
+}
+
+puts preferred_greetings[current_time_range]
