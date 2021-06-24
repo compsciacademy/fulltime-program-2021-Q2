5d25cab10688943efff5944cac2bb67393dfecdb
Drew Ogryzek <drew@vancouvertechpodcast.ca>
2021-06-24 14:09:07 -0700

    Rails Intro: Part 1 --- CRUD

--- Week09/Day44/blog/config/routes.rb
+++ Week09/Day44/blog/config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   # get '/articles', to: 'articles#index'
   # get '/articles/:id', to: 'articles#show'
-  resources :articles, only: [:index, :show]
+  # resources :articles, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :articles
 end
